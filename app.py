5689c6ef1892caf855b46cc719ecaf41e66a57ef
FangWei Huang <amwawadgt@gmail.com>
2018-05-06 17:07:34 +0900

    add creator

---
+++ app.py
@@ -0,0 +1,30 @@
+import json
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route("/")
+def index():
+    with open('db.json') as f:
+        db = json.load(f)
+
+    response = render_template("index.html", setting=db["setting"],
+                               sliders=db["sliders"], categories=db["categories"])
+    with open('index.html', 'w') as f:
+        f.write(response)
+
+    return response
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
